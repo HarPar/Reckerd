cdb5227d4e1f89474785fda1adf0eb8622cc0610
Harshil Parikh <harshil.parikh@uwaterloo.ca>
2018-02-03 03:39:12 -0500

    Initial Commit

---
+++ Pods/Target Support Files/Pods-receipt-trackerTests/Pods-receipt-trackerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_receipt_trackerTests : NSObject
+@end
+@implementation PodsDummy_Pods_receipt_trackerTests
+@end
---
+++ receipt-tracker/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  receipt-tracker
+//
+//  Created by Harshil Parikh on 2018-02-03.
+//  Copyright © 2018 QHacks 2018. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import SwiftyCam
+
+class ViewController: SwiftyCamViewController {
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+
+    @IBOutlet weak var previewLayer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        /*let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewLayer.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+        } catch {
+            print(error)
+        }*/
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
