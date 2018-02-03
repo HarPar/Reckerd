d0b4d0c793feea94819cabb386962c7a4e75c798
Harshil Parikh <harshil.parikh@uwaterloo.ca>
2018-02-03 10:01:12 -0500

    image

--- receipt-tracker/ViewController.swift
+++ receipt-tracker/ViewController.swift
@@ -8,23 +8,19 @@
 
 import UIKit
 import AVFoundation
-import Vision
+import Alamofire
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+class ViewController: UIViewController {
     
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    
-    var requests = [VNRequest]()
+    let photoOutput = AVCapturePhotoOutput()
 
     @IBOutlet weak var shutter: UIButton!
     @IBOutlet weak var previewLayer: UIView!
     
-    var devicePosition: AVCaptureDevice.Position = .back
+    var capturedImage : UIImage?
 
-    
-    @IBAction func shutterClicked(_ sender: Any) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -48,51 +44,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, AVCaptureVi
         }
         
         self.previewLayer.bringSubview(toFront: shutter)
-        
-        let rectangleDetectionRequest = VNDetectRectanglesRequest(completionHandler: handleRectangles)
-        rectangleDetectionRequest.minimumSize = 0.1
-        rectangleDetectionRequest.maximumObservations = 1
-        
-        self.requests = [rectangleDetectionRequest]
 
-    }
-    
-    func handleRectangles(request: VNRequest, error: Error?) {
-        DispatchQueue.main.async {
-            self.drawVisionRequestResults(request.results as! [VNRectangleObservation])
-        }
-    }
-    
-    func drawVisionRequestResults (_ results: [VNRectangleObservation]) {
-        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -self.previewLayer.frame.height)
-        
-        let translate = CGAffineTransform.identity.scaledBy(x: self.previewLayer.frame.width, y: self.previewLayer.frame.height)
-        
-        for rectangle in results {
-            let rectangleBounds = rectangle.boundingBox.applying(translate).applying(transform)
-            previewLayer.draw(rectangleBounds)
-            
-        }
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
-        
-        var requestOptions:[VNImageOption:Any] = [:]
-        
-        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
-            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
-        }
-        
-        let o = self.exifOrientationFromDeviceOrientation()
-        
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue:UInt32(o))!, options: requestOptions)
-        
-        do {
-            try imageRequestHandler.perform(self.requests)
-        } catch {
-            print(error)
-        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -100,33 +52,85 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, AVCaptureVi
         // Dispose of any resources that can be recreated.
     }
     
-    func exifOrientationFromDeviceOrientation() -> Int32 {
-        enum DeviceOrientation: Int32 {
-            case top0ColLeft = 1
-            case top0ColRight = 2
-            case bottom0ColRight = 3
-            case bottom0ColLeft = 4
-            case left0ColTop = 5
-            case right0ColTop = 6
-            case right0ColBottom = 7
-            case left0ColBottom = 8
-        }
-        var exifOrientation: DeviceOrientation
+    @IBAction func shutterClicked(_ sender: Any) {
+        let photoSettings = AVCapturePhotoSettings()
+        photoSettings.isHighResolutionPhotoEnabled = true
         
-        switch UIDevice.current.orientation {
-        case .portraitUpsideDown:
-            exifOrientation = .left0ColBottom
-        case .landscapeLeft:
-            exifOrientation = devicePosition == .front ? .bottom0ColRight : .top0ColLeft
-        case .landscapeRight:
-            exifOrientation = devicePosition == .front ? .top0ColLeft : .bottom0ColRight
-        default:
-            exifOrientation = .right0ColTop
+        if let firstAvailablePreviewPhotoPixelFormatTypes = photoSettings.availablePreviewPhotoPixelFormatTypes.first {
+            photoSettings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: firstAvailablePreviewPhotoPixelFormatTypes]
         }
-        return exifOrientation.rawValue
+        
+        photoOutput.capturePhoto(with: photoSettings, delegate: self as! AVCapturePhotoCaptureDelegate)
     }
+}
 
-
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    
+    static func post(image: UIImage, for username: String) {
+        
+        let imageData = UIImagePNGRepresentation(image)
+        let base64Image = imageData?.base64EncodedString(options: .lineLength64Characters)
+        
+        let url = "https://api.imgur.com/3/upload"
+        
+        let parameters = [
+            "image": base64Image
+        ]
+        
+        Alamofire.upload(multipartFormData: { multipartFormData in
+            if let imageData = UIImageJPEGRepresentation(image, 1) {
+                multipartFormData.append(imageData, withName: username, fileName: "\(username).png", mimeType: "image/png")
+            }
+            
+            for (key, value) in parameters {
+                multipartFormData.append((value?.data(using: .utf8))!, withName: key)
+            }}, to: url, method: .post, headers: ["Authorization": "Client-ID " + "42e89db92aac10d"],
+                encodingCompletion: { encodingResult in
+                    switch encodingResult {
+                    case .success(let upload, _, _):
+                        upload.response { response in
+                            //This is what you have been missing
+                            let json = try? JSONSerialization.jsonObject(with: response.data!, options: .allowFragments) as! [String:Any]
+                            print(json)
+                            let imageDic = json?["data"] as? [String:Any]
+                            print(imageDic?["link"])
+                        }
+                    case .failure(let encodingError):
+                        print("error:\(encodingError)")
+                    }
+        })
+        
+    }
+    
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let error = error {
+            print("Error capturing photo: \(error)")
+        } else {
+            if let sampleBuffer = photoSampleBuffer, let previewBuffer = previewPhotoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+                
+                if let image = UIImage(data: dataImage) {
+                    self.capturedImage = image
+                    print("Poting to imgur")
+                    ViewController.post(image: self.capturedImage!, for: "harparrr")
+                }
+            }
+        }
+        
+    }
+    
+    @available(iOS 11.0, *)
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let data = photo.fileDataRepresentation(),
+            let image =  UIImage(data: data)  else {
+                return
+        }
+        
+        self.capturedImage = image
+        print("Posting to imgur2")
+        ViewController.post(image: self.capturedImage!, for: "harparrr")
+    }
 }
 
 extension UIView {
