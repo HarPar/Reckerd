c804984616cbdf1e83b2b111d4c704eed04ca2e7
Harshil Parikh <harshil.parikh@uwaterloo.ca>
2018-02-03 05:25:59 -0500

    draw

--- receipt-tracker/ViewController.swift
+++ receipt-tracker/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    var requests = [VNRequest]()
 
     @IBOutlet weak var shutter: UIButton!
     @IBOutlet weak var previewLayer: UIView!
@@ -42,7 +45,31 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         }
         
         self.previewLayer.bringSubview(toFront: shutter)
+        
+        let rectangleDetectionRequest = VNDetectRectanglesRequest(completionHandler: handleRectangles)
+        rectangleDetectionRequest.minimumSize = 0.1
+        rectangleDetectionRequest.maximumObservations = 1
+        
+        self.requests = [rectangleDetectionRequest]
 
+    }
+    
+    func handleRectangles(request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            self.drawVisionRequestResults(request.results as! [VNRectangleObservation])
+        }
+    }
+    
+    func drawVisionRequestResults (_ results: [VNRectangleObservation]) {
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -self.previewLayer.frame.height)
+        
+        let translate = CGAffineTransform.identity.scaledBy(x: self.previewLayer.frame.width, y: self.previewLayer.frame.height)
+        
+        for rectangle in results {
+            let rectangleBounds = rectangle.boundingBox.applying(translate).applying(transform)
+            previewLayer.draw(rectangleBounds)
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
